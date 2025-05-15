d42c8a5898f86fef2889d8a0b8a8e750bb734614
larajeraj <132908556+larajeraj@users.noreply.github.com>
2025-05-15 10:22:28 +0200

    auth in gitignore

--- DATA/auth_public.py
+++ DATA/auth_public.py
@@ -1,5 +1,5 @@
 # Skopiraj datoteko v auth.py in vnesi podatke za priklop na bazo
 db = 'sem2025_jerajla'
 host = 'baza.fmf.uni-lj.si'
-user = 'javno'
-password = 'geslo'
+user = 'jerajla'
+password = '5tb6vmqm'
