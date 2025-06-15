615877c2287b0377921e714a4631ff2f5e42f84d
larajeraj <132908556+larajeraj@users.noreply.github.com>
2025-06-15 21:04:36 +0200

    gref_stanovanja_po_regijah

--- Presentation/charts.py
+++ Presentation/charts.py
@@ -58,3 +58,32 @@ def graf_vse_po_regijah(df: pd.DataFrame, selected_regions: list, leto_od: int, 
     fig.update_yaxes(title_text="Delovno aktivno prebivalstvo", secondary_y=True)
 
     return fig
+
+def graf_stanovanja_po_regijah(df: pd.DataFrame, selected_regije: list, leto_od: int, leto_do: int):
+    dff = df[df["regija"].isin(selected_regije)].copy()
+
+    if dff.empty:
+        return go.Figure().update_layout(title="Ni podatkov za izbrane regije.")
+
+    dff["leto"] = pd.to_datetime(dff["leto"], format="%Y")
+    dff = dff[(dff["leto"].dt.year >= leto_od) & (dff["leto"].dt.year <= leto_do)]
+
+    if dff.empty:
+        return go.Figure().update_layout(title="Ni podatkov za izbrana leta.")
+    
+    dff["leto_leto"] = dff["leto"].dt.year
+
+    # Agregira po letu in številu sob (regije se združijo)
+    df_agg = dff.groupby(["leto_leto", "sobe"], as_index=False)["stevilo"].sum()
+
+    fig = px.bar(df_agg,
+                 x="leto",
+                 y="stevilo",
+                 color="sobe",
+                 barmode="group",  ,
+                 title=f"Število stanovanj po občinah za obdobje {leto_od} do {leto_do}",
+                 labels={"stevilo": "Število stanovanj", "regija": "Regija"})
+
+    fig.update_layout(barmode='group', xaxis_title="Leto", yaxis_title="Število stanovanj")
+
+    return fig
